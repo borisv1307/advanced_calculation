468ef162a0065716a24e6ceda689b40b03468228
Greg Warchol <gre8ory1@gmail.com>
2020-11-10 19:49:39 -0500

    Added test for advanced calculator

--- lib/advanced_calculator.dart
+++ lib/advanced_calculator.dart
@@ -4,15 +4,29 @@ import 'package:advanced_calculation/src/calculator.dart';
 import 'src/coordinate_calculator.dart';
 
 class AdvancedCalculator {
-  CoordinateCalculator coordinateCalculator = CoordinateCalculator();
-  Calculator calculator = Calculator();
+  CoordinateCalculator _coordinateCalculator;
+  Calculator _calculator;
+
+  AdvancedCalculator(){
+    _coordinateCalculator = getCoordinateCalculator();
+    _calculator = getCalculator();
+  }
+
 
   double calculateEquation(String equation, double xValue){
-    return coordinateCalculator.calculate(equation, xValue);
+    return _coordinateCalculator.calculate(equation, xValue);
   }
 
   String calculate(String input){
-    return calculator.calculate(input);
+    return _calculator.calculate(input);
+  }
+
+  CoordinateCalculator getCoordinateCalculator(){
+    return CoordinateCalculator();
+  }
+
+  Calculator getCalculator(){
+    return Calculator();
   }
 
 }
--- test/advanced_calculator_test.dart
+++ test/advanced_calculator_test.dart
@@ -1,4 +1,54 @@
 
-void main() {
+import 'package:advanced_calculation/advanced_calculator.dart';
+import 'package:advanced_calculation/src/calculator.dart';
+import 'package:advanced_calculation/src/coordinate_calculator.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
+class MockCoordinateCalculator extends Mock implements CoordinateCalculator{}
+class MockCalculator extends Mock implements Calculator{}
+
+class TestableAdvancedCalculator extends AdvancedCalculator{
+  CoordinateCalculator coordinateCalculator;
+  Calculator calculator;
+
+  TestableAdvancedCalculator(this.calculator, this.coordinateCalculator);
+
+  @override
+  CoordinateCalculator getCoordinateCalculator(){
+    return coordinateCalculator;
+  }
+  @override
+  Calculator getCalculator(){
+    return calculator;
+  }
+}
+
+void main() {
+  MockCoordinateCalculator mockCoordinateCalculator;
+  AdvancedCalculator advancedCalculator;
+  Calculator mockCalculator;
+
+  setUp((){
+    mockCoordinateCalculator = MockCoordinateCalculator();
+    mockCalculator = MockCalculator();
+
+    advancedCalculator = TestableAdvancedCalculator(mockCalculator,mockCoordinateCalculator);
+  });
+
+  test('Coordinate calculator is utilized',(){
+    when(mockCoordinateCalculator.calculate('x', 1)).thenReturn(2);
+
+     double output = advancedCalculator.calculateEquation('x', 1);
+     expect(output, 2);
+     verify(mockCoordinateCalculator.calculate('x', 1)).called(1);
+  });
+
+  test('Calculator is utilized',(){
+    when(mockCalculator.calculate('1+1')).thenReturn('2');
+
+    String output = advancedCalculator.calculate('1+1');
+    expect(output, '2');
+    verify(mockCalculator.calculate('1+1')).called(1);
+  });
 }
