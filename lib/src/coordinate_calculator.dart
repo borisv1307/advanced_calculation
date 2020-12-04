dbcf17adfbd5c3fa8c22c9656bda31f465341c1b
Ian Franklin <ianaf100@gmail.com>
2020-12-04 18:09:48 -0500

    translator support for x

--- lib/src/coordinate_calculator.dart
+++ lib/src/coordinate_calculator.dart
@@ -1,19 +1,18 @@
 import 'package:advanced_calculation/src/library_loader.dart';
+import 'package:advanced_calculation/src/translator/translator.dart';
 import 'package:ffi/ffi.dart';
 
 class CoordinateCalculator {
-  GraphCalculateFunction calculateFunction;
+  final GraphCalculateFunction calculateFunction;
+  final Translator translator;
 
-  CoordinateCalculator(){
-    calculateFunction = getLibraryLoader().loadGraphCalculateFunction();
-  }
+  CoordinateCalculator({LibraryLoader loader, Translator translator}):
+    calculateFunction = (loader ?? LibraryLoader()).loadGraphCalculateFunction(),
+    translator = translator ?? Translator();
 
-  LibraryLoader getLibraryLoader(){
-    return LibraryLoader();
-  }
 
   double calculate(String equation, double xValue){
-    double result = calculateFunction(Utf8.toUtf8(equation), xValue);
-    return result;
+    String translated = translator.translate(equation);
+    return calculateFunction(Utf8.toUtf8(translated), xValue);
   }
 }
