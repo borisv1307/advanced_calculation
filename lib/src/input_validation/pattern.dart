ceac716429e661fc4a611d82d95a2ac74eaac220
Ian Franklin <ianaf100@gmail.com>
2020-12-04 22:25:06 -0500

    fix negative symbols

--- lib/src/input_validation/pattern.dart
+++ lib/src/input_validation/pattern.dart
@@ -1,11 +1,11 @@
 // This class represents the possible Regex patterns used for input validation
 class Pattern {
   static final validOperand = RegExp(r'^(\d*\.?\d+)?(𝜋|𝑒|²|⁻¹)*$', unicode: true);
-  static final validAllOperator = RegExp(r'^[,+\-\/*^)]$');
+  static final validAllOperator = RegExp(r'^[,+\−\/*^)]$');
   static final validNoPlusMinusOperator = RegExp(r'^[)^,*\/]$');
   static final validBasicOperator = RegExp(r'^[+\−\/*^]$');
-  static final validCommaBasicOperator = RegExp(r'^[,+\-\/*^]$');
-  static final validOperator = RegExp(r'^[+\-\/*]$');
-  static final addSubtractOperator = RegExp(r'^[+\-]$');
+  static final validCommaBasicOperator = RegExp(r'^[,+\−\/*^]$');
+  static final validOperator = RegExp(r'^[+\−\/*]$');
+  static final addSubtractOperator = RegExp(r'^[+\−]$');
   static final multiplyDivideOperator = RegExp(r'^[*\/]$');
 }
