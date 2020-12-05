62cff805c3e98cc87ed6ea9a9fcf5afcf2863e18
Ian Franklin <ianaf100@gmail.com>
2020-12-05 15:24:16 -0500

    update tests

--- lib/src/coordinate_calculator.dart
+++ lib/src/coordinate_calculator.dart
@@ -3,14 +3,18 @@ import 'package:advanced_calculation/src/translator/translator.dart';
 import 'package:ffi/ffi.dart';
 
 class CoordinateCalculator {
-  final GraphCalculateFunction calculateFunction;
-  final Translator translator;
+  GraphCalculateFunction calculateFunction;
+  Translator translator;
 
-  CoordinateCalculator({LibraryLoader loader, Translator translator}):
-    calculateFunction = (loader ?? LibraryLoader()).loadGraphCalculateFunction(),
+  CoordinateCalculator({LibraryLoader loader, Translator translator}) {
+    calculateFunction = (loader ?? getLibraryLoader()).loadGraphCalculateFunction();
     translator = translator ?? Translator();
+  }
 
-
+  LibraryLoader getLibraryLoader(){
+    return LibraryLoader();
+  }
+  
   double calculate(String equation, double xValue){
     String translated = translator.translate(equation);
     return calculateFunction(Utf8.toUtf8(translated), xValue);
